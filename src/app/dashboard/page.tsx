6efb44a5842b538d245dbb2311fcde71413b3a16
Alexander Perez <alexander.pereze02@gmail.com>
2023-06-09 19:40:01 -0400

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/ui/Button';
+import React, { FunctionComponent } from 'react';
+
+interface pageProps {}
+ 
+const page: FunctionComponent<pageProps> = () => {
+  return <Button>Hello</Button>
+}
+ 
+export default page;
